b3c38640e9b913da053b02374dd57df5de28391e
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-17 10:28:03 +0300

    Created test for MutableLiveStream

---
+++ src/test/java/com/BuildFirstRestAPI/FirstRestAPI/model/LiveStreamTest.java
@@ -0,0 +1,28 @@
+package com.BuildFirstRestAPI.FirstRestAPI.model;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+public class LiveStreamTest {
+
+    @Test
+    void createNewMutableLiveStream() {
+        MutableLiveStream stream = new MutableLiveStream();
+        stream.setId(UUID.randomUUID().toString());
+        stream.setTitle("Building REST APIs with Spring Boot");
+        stream.setDescription("""
+                Spring Boot is very convenient to use when
+                building REST APIs;
+                """);
+        stream.setUrl("https://github.com/NMKrastev/FirstRestAPI.git");
+        stream.setStartDate(LocalDateTime.of(2022, 6, 20, 19, 30));
+        stream.setEndDate(LocalDateTime.of(2022, 6, 20, 21, 30));
+
+        assertNotNull(stream);
+        assertEquals("Title is incorrect", "Building REST APIs with Spring Boot", stream.getTitle());
+
+    }
+}

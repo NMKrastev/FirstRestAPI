ae43b96e826dc48eb70f771f3f83d6810870efd7
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-17 10:42:25 +0300

    Added test in LiveStreamTest for ImmutableLiveStream

--- src/test/java/com/BuildFirstRestAPI/FirstRestAPI/model/LiveStreamTest.java
+++ src/test/java/com/BuildFirstRestAPI/FirstRestAPI/model/LiveStreamTest.java
@@ -1,8 +1,10 @@
 package com.BuildFirstRestAPI.FirstRestAPI.model;
 
 import org.junit.jupiter.api.Test;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
+
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
@@ -20,6 +22,24 @@ public class LiveStreamTest {
         stream.setUrl("https://github.com/NMKrastev/FirstRestAPI.git");
         stream.setStartDate(LocalDateTime.of(2022, 6, 20, 19, 30));
         stream.setEndDate(LocalDateTime.of(2022, 6, 20, 21, 30));
+
+        assertNotNull(stream);
+        assertEquals("Title is incorrect", "Building REST APIs with Spring Boot", stream.getTitle());
+
+    }
+
+    @Test
+    void createNewImmutableLiveStream() {
+        ImmutableLiveStream stream = new ImmutableLiveStream(
+                UUID.randomUUID().toString(),
+                "Building REST APIs with Spring Boot",
+                """
+                        Spring Boot is very convenient to use when
+                        building REST APIs;
+                     """,
+                "https://github.com/NMKrastev/FirstRestAPI.git",
+                LocalDateTime.of(2022, 6, 20, 19, 30),
+                LocalDateTime.of(2022, 6, 20, 21, 30));
 
         assertNotNull(stream);
         assertEquals("Title is incorrect", "Building REST APIs with Spring Boot", stream.getTitle());

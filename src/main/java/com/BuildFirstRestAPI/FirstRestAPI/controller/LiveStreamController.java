02700e4e80020c90adea469133c62f60b765d03f
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-17 12:04:30 +0300

    Updated LiveStreamController class

--- src/main/java/com/BuildFirstRestAPI/FirstRestAPI/controller/LiveStreamController.java
+++ src/main/java/com/BuildFirstRestAPI/FirstRestAPI/controller/LiveStreamController.java
@@ -1,6 +1,7 @@
 package com.BuildFirstRestAPI.FirstRestAPI.controller;
 
 import com.BuildFirstRestAPI.FirstRestAPI.model.LiveStream;
+import com.BuildFirstRestAPI.FirstRestAPI.repository.LiveStreamRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,15 @@ import java.util.List;
 @RequestMapping("/streams")
 public class LiveStreamController {
 
-    //http://localhost:8090/streams
+    private final LiveStreamRepository repository;
+
+    public LiveStreamController(LiveStreamRepository repository) {
+        this.repository = repository;
+    }
+
+    // GET http://localhost:8090/streams
     @GetMapping
     public List<LiveStream> findAll() {
-        return null;
+        return repository.findAll();
     }
 }

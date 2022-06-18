5ba08ae6c25f090b7daca500ceade4b059e46063
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-18 15:07:21 +0300

    Upadated LiveStream class so now it validates if the title ot the stream is empty

--- src/main/java/com/BuildFirstRestAPI/FirstRestAPI/model/LiveStream.java
+++ src/main/java/com/BuildFirstRestAPI/FirstRestAPI/model/LiveStream.java
@@ -1,9 +1,16 @@
 package com.BuildFirstRestAPI.FirstRestAPI.model;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
-public record LiveStream(String id, String title, String description, String url, LocalDateTime startDate, LocalDateTime endDate) {
+public record LiveStream(
+        String id,
+        @NotEmpty
+        String title,
+        String description,
+        String url,
+        LocalDateTime startDate,
+        LocalDateTime endDate) {
 
 
 }
-
